47061ba1e4780ab6b64ead5191d3a10406db3bc0
shin-hama <htysivista@gmail.com>
2021-06-05 21:11:44 +0900

    MAINT: make to pass mypy

--- app/db/memos.py
+++ app/db/memos.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
 
-from app.models import models, schemas
+from app.models import models
 
 
-def get_memo(db: Session, memo_id: int) -> schemas.Memo:
+def get_memo(db: Session, memo_id: int):
     return db.query(models.Memo).filter(models.Memo.id == memo_id).first()
 
 
 def get_memos(
     db: Session, skip: int = 0, limit: int = 100
-) -> list[schemas.Memo]:
+):
     return db.query(models.Memo).offset(skip).limit(limit).all()
