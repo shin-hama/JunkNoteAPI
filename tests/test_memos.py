78cc9c858737a8eee97b2e33a9b31c2fefe85fe9
shin-hama <htysivista@gmail.com>
2021-06-11 22:07:04 +0900

    WIP: Add unittest

---
+++ tests/test_memos.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from starlette.status import HTTP_404_NOT_FOUND
+
+
+class TestHedgehogsRoutes:
+    def test_routes_exist(
+        self,
+        client: TestClient
+    ) -> None:
+        res = client.get("/")
+        assert res.status_code != HTTP_404_NOT_FOUND
