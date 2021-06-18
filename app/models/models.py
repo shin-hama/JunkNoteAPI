2e073e482b5cac56a2d5451a17fe17cd9dd5a237
shin-hama <htysivista@gmail.com>
2021-06-18 16:10:06 +0900

    FEAT: Migrate to change text column

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+import sqlalchemy as sa
 from sqlalchemy.orm import relationship
 
 from app.db.db import Base
@@ -7,11 +7,11 @@ from app.db.db import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(128), index=True)
-    hashed_password = Column(String(32))
-    username = Column(String(32))
-    created_at = Column(DateTime, index=True)
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+    email = sa.Column(sa.Text, index=True)
+    hashed_password = sa.Column(sa.String(60))
+    username = sa.Column(sa.Text)
+    created_at = sa.Column(sa.DateTime, index=True)
 
     memos = relationship("Memo", back_populates="owner")
 
@@ -19,11 +19,11 @@ class User(Base):
 class Memo(Base):
     __tablename__ = "memos"
 
-    id = Column(Integer, primary_key=True, index=True)
-    containts = Column(String(2000))
-    datetime = Column(DateTime, index=True)
-    reference = Column(String(2000))
-    owner_id = Column(Integer, ForeignKey("users.id"))
-    is_removed = Column(Boolean, default=False, index=True)
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+    containts = sa.Column(sa.Text)
+    datetime = sa.Column(sa.DateTime, index=True)
+    reference = sa.Column(sa.Text)
+    owner_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
+    is_removed = sa.Column(sa.Boolean, default=False, index=True)
 
     owner = relationship("User", back_populates="memos")
