8615d28dc8a182c1ad60a5e20229875ba1d70b24
shin-hama <htysivista@gmail.com>
2021-07-28 15:53:48 +0900

    TEST: Add authentication service test.

---
+++ tests/test_services/test_authentication.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import false
+
+from app.models import models
+from app.services.authentication import check_email_is_taken
+
+
+def test_email_is_taken(
+    test_user: models.User,
+    db_session: Session,
+):
+    is_taken = check_email_is_taken(db_session, test_user.email)
+    assert is_taken
+
+
+def test_email_is_not_taken(
+    test_user: models.User,
+    db_session: Session,
+):
+    no_taken_email = "no_taken_email@example.com"
+    is_taken = check_email_is_taken(db_session, no_taken_email)
+    assert is_taken is False
